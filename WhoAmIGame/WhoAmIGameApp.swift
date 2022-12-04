4bb66cfffc533943158fcac9bd851f03ac9f54f3
Richie <richard1simonik@gmail.com>
2022-12-04 14:32:02 +0100

    Initial Commit

---
+++ WhoAmIGame/WhoAmIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAmIGameApp.swift
+//  WhoAmIGame
+//
+//  Created by Richard on 04.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhoAmIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
