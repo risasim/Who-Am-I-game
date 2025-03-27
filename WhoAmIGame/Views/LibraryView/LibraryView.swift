5c3c0f4541eb19b4fb1a1d3e40e2379a6b104469
Richard <richard1simonik@gmail.com>
2025-03-27 17:12:02 +0100

    fixed the onboarding of the library

--- WhoAmIGame/Views/LibraryView/LibraryView.swift
+++ WhoAmIGame/Views/LibraryView/LibraryView.swift
@@ -12,33 +12,36 @@ struct LibraryView: View {
     @State var isSettingsShown: Bool = false
     @AppStorage("libraryExplanationShown") var libShown: Bool = false
     var body: some View {
-        if(!libShown){
-            LibraryOnBoardingView()
-        }else{
-            NavigationStack{
-                ChooseConnectionView()
-                    .sheet(isPresented: $isSettingsShown, content: {
-                        SettingsView()
-                    })
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Button {
-                                isSettingsShown = true
-                            } label: {
-                                Image(systemName: "gear")
-                                    .font(.title2)
-                                    .padding(.bottom,0)
-                            }
-                        }
-                        ToolbarItem(placement: .principal) {
-                            Text("Who Am I ?")
-                                .font(.system(size: 27, weight: .bold, design: .rounded))
-                                .padding(.bottom, 0)
-                        }
-                        
-                    }
-                    .navigationBarTitleDisplayMode(.inline)
+        
+        NavigationStack{
+            VStack{
+                if(!libShown){
+                    LibraryOnBoardingView()
+                }else{
+                    ChooseConnectionView()
+                }
             }
+            .sheet(isPresented: $isSettingsShown, content: {
+                SettingsView()
+            })
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        isSettingsShown = true
+                    } label: {
+                        Image(systemName: "gear")
+                            .font(.title2)
+                            .padding(.bottom,0)
+                    }
+                }
+                ToolbarItem(placement: .principal) {
+                    Text("Who Am I ?")
+                        .font(.system(size: 27, weight: .bold, design: .rounded))
+                        .padding(.bottom, 0)
+                }
+                
+            }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
